6149872d25cd5accae0cab323710ba32f58c024e
Tomi Paananen <tomi.paananen@microsoft.com>
2017-05-12 15:08:13 +0300

    Minor change.

--- IntermediatorBotSample/MessageRouting/AzureTableStorageRoutingDataManager.cs
+++ IntermediatorBotSample/MessageRouting/AzureTableStorageRoutingDataManager.cs
@@ -1,10 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Web;
 using Microsoft.Bot.Connector;
 using Microsoft.WindowsAzure.Storage;
 using Microsoft.Azure;
+using Underscore.Bot.MessageRouting;
 
 namespace IntermediatorBotSample.MessageRouting
 {
