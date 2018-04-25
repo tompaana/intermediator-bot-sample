630199e931498d4cb53bba988e2286d7dd49c0f1
Tomi Paananen <tomi.paananen@microsoft.com>
2018-04-25 10:26:04 -0700

    Adds the basic bot code.

---
+++ IntermediatorBotSample/IntermediatorBot.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Bot;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+using System;
+using System.Threading.Tasks;
+
+namespace IntermediatorBotSample
+{
+    public class IntermediatorBot : IBot
+    {
+        public async Task OnTurn(ITurnContext context)
+        {
+            if (context.Activity.Type is ActivityTypes.Message)
+            {
+                await context.SendActivity($"Hello world.");
+            }
+        }
+    }
+}
--- IntermediatorBotSample/Startup.cs
+++ IntermediatorBotSample/Startup.cs
@@ -1,20 +1,47 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using IntermediatorBotSample.Settings;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Bot.Builder.BotFramework;
+using Microsoft.Bot.Builder.Integration.AspNet.Core;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace IntermediatorBotSample
 {
     public class Startup
     {
+        public static BotSettings Settings
+        {
+            get;
+            private set;
+        }
+
+        public IConfiguration Configuration
+        {
+            get;
+        }
+
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(env.ContentRootPath)
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                .AddEnvironmentVariables();
+            Configuration = builder.Build();
+
+            Settings = new BotSettings(Configuration);
+        }
+
         // This method gets called by the runtime. Use this method to add services to the container.
-        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSingleton(_ => Configuration);
+            services.AddBot<IntermediatorBot>(options =>
+            {
+                options.CredentialProvider = new ConfigurationCredentialProvider(Configuration);
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +52,9 @@ namespace IntermediatorBotSample
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+            app.UseDefaultFiles();
+            app.UseStaticFiles();
+            app.UseBotFramework();
         }
     }
 }
