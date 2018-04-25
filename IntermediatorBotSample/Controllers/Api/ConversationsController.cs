ff9bef378bec6071b8c0e71b7f0451df80d85de8
pedias <pedias@microsoft.com>
2018-04-25 16:57:11 -0700

    Add initial versions of ConversationController and MessagesController

--- IntermediatorBotSample/Controllers/Api/ConversationsController.cs
+++ IntermediatorBotSample/Controllers/Api/ConversationsController.cs
@@ -2,8 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using FizzWare.NBuilder;
+using IntermediatorBotSample.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Bot.Schema;
 using Newtonsoft.Json;
 
 namespace IntermediatorBotSample.Controllers.Api
@@ -12,15 +15,15 @@ namespace IntermediatorBotSample.Controllers.Api
     public class ConversationsController : Controller
     {
         [HttpGet]
-        public IEnumerable<string> Get()
+        public IEnumerable<Conversation> Get()
         {
-            var stupidResult = new
-            {
-                Name   = "Jorge",
-                Value  = "Hello World",
-                Reason = "Cuz I can!"
-            };
-            yield return JsonConvert.SerializeObject(stupidResult);
+            return Builder<Conversation>.CreateListOfSize(2)
+                .All()
+                    .With(o =>  o.ConversationInformation   = Builder<ConversationInformation>.CreateNew().Build())
+                    .With(o =>  o.ConversationReference     = Builder<ConversationReference>.CreateNew().Build())
+                    .With(o =>  o.UserInformation           = Builder<UserInformation>.CreateNew().Build())
+                .Build()
+                .ToList();
         }
     }
 }
---
+++ IntermediatorBotSample/Models/ConversationInformation.cs
@@ -0,0 +1,23 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace IntermediatorBotSample.Models
+{
+    [JsonObject(Title ="conversationinformation")]
+    public class ConversationInformation
+    {
+        [JsonProperty(PropertyName = "intialdate")]
+        public DateTime InitialDate { get; set; }
+
+
+        [JsonProperty(PropertyName = "sentimentscore")]
+        public double SentimentScore { get; set; }
+
+
+        [JsonProperty(PropertyName = "messagescount")]
+        public int MessagesCount { get; set; }
+    }
+}
